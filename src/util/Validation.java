9cb7d60778297edfe7f1e572535eff9a63822348
Ayush Srivastav <ayushmanoj9936@gmail.com>
2026-01-19 18:24:38 +0530

    Update Validation.java

--- src/util/Validation.java
+++ src/util/Validation.java
@@ -4,5 +4,5 @@ import Exceptions.ValidationException;
 
 @FunctionalInterface
 public interface Validation<T> {
-    void validate(T value) throws ValidationException; // T means Generic type which works as a placeholder which tells the java that here the value is not fixed,this functional interface can work for multiple datatypes like string ,double etc
+    void validate(T value) throws ValidationException; // T means Generic type which works as a placeholder which tells the java that here the value is not fixed and this functional interface can work for multiple datatypes like string ,double etc
 }
